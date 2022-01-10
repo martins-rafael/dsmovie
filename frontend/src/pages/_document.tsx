969bf7d9a8c6c02815631907e0b524970b98e933
Rafael Martins <rafaeldcmartins@gmail.com>
2022-01-10 10:50:32 -0300

    Project clean

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  };
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <meta name="theme-color" content="#3d4451" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>DSMovie</title>
+        <meta
+          name="description"
+          content="Aplicação desenvolvida na Semana Spring React"
+        />
+      </Head>
+
+      <h1>Página principal</h1>
+    </>
+  );
+};
+
+export default Home;
